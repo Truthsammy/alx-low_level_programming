c14eac1a1681700aeefc1a4c8f5994be8fe4f7e0
Truthsammy <nelsonsamson80@gmail.com>
2022-12-11 07:27:49 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,14 +6,18 @@
  */
 int main(void)
 {
-	int num;
-	char la;
+	int i;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
 
-	for (la = 'a'; la <= 'f'; la++)
-		putchar(la);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
 
 
 	putchar('\n');
