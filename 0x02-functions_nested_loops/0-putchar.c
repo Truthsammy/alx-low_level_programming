629513d1462d5e41f223712f6b7283ceb97b2aa4
Truthsammy <nelsonsamson80@gmail.com>
2022-12-14 13:06:07 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * is writen by samtech
- * Return: 0
+ * main -  prints _putchar, followed by a new line
+ * Return always 0
  */
 int main(void)
 {
@@ -15,6 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+
 	_putchar('\n');
 
 	return (0);
