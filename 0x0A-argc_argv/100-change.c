fcb1305aaef755e410959de485d612d0c4e96562
Truthsammy <nelsonsamson80@gmail.com>
2023-01-31 09:37:58 -0800

    argc_agv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - prints the minimum number of coins
- * @argc: The number of arguments supllied to the program.
- * @argv: An array of pointers to the arguments.
- * Return: if the atgument is not one -1 othereise -0.
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int cents, ncoins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -21,30 +21,18 @@ int main(int argc, char *argv[])
 
 	while (cents > 0)
 	{
-		coins++;
-		if ((cents - 25) >= 0)
-		{
+		if (cents >= 25)
 			cents -= 25;
-			continue;
-		}
-		if ((cents - 10) >= 0)
-		{
+		else if (cents >= 10)
 			cents -= 10;
-			continue;
-		}
-		if ((cents - 5) >= 0)
-		{
+		else if (cents >= 5)
 			cents -= 5;
-			continue;
-		}
-		if ((cents - 2) >= 0)
-		{
+		else if (cents >= 2)
 			cents -= 2;
-			continue;
-		}
-		cents--;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
-	printf("%d\n", coins);
-
+	printf("%d\n", ncoins);
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - Prints the multiplication of teo numbers, followed by a nee line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- * Return: if the program receives teo arguments -0.
- * if the program does not receive two argument -1.
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, prod;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	prod = num1 * num2;
-
-	printf("%d\n", prod);
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
